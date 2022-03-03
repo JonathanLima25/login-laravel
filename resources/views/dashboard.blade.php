f2c9d849bd5412e221228169a6ec5c5186330db1
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-03-03 19:49:48 -0300

    migrate table person

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     @can('user')
-                        Dados de usuário
+                        <h1 class="text-center">Dados de usuário</h1>
                     @elsecan('admin')
                         Somente o administrador!
                     @endcan
